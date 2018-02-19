06783e395a1c934cd110704c77be179c0d71f7f9
Srushtika <n.srushtika@gmail.com>
2018-02-19 11:41:53 +0100

    chart working with static data

--- ng-vote-ably/src/app/vote-chart/vote-chart.component.ts
+++ ng-vote-ably/src/app/vote-chart/vote-chart.component.ts
@@ -15,7 +15,8 @@ export class VoteChartComponent implements OnInit {
 
 	// Ably attributes
 	ably : any
-	receiveChannel : any
+  receiveChannel : any
+  chart = []
 
 	ngOnInit() {
 		this.ably = new Ably.Realtime('6QdTiA.O9vBCw:8glD3fRTNhSEVJC5')
@@ -24,56 +25,38 @@ export class VoteChartComponent implements OnInit {
 		this.receiveChannel = this.ably.channels.get('vote-channel')
     console.log(this.receiveChannel)
 		// Ably Subscription
-		this.receiveChannel.subscribe( function(message:any) {
-      //this.reply = message.data
-     // console.log('here')
-
-//chart begin
-let canvas: any = document.getElementById("myChart");
-/*let ctx: any = document.getElementById("myChart").getContext('2d');
-var parent = document.getElementById("parent");
-canvas.width = parent.offsetWidth;
-canvas.height = parent.offsetHeight;
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-//chart end
-*/
-
+		this.receiveChannel.subscribe(function(message: any) {
       console.log(JSON.stringify(message.data))
+      this.chart= new Chart('canvas', {
+        type: 'bar',
+        data: {
+          labels: ["Yes", "No", "MayBe"],
+          datasets: [{
+              label: 'Is this conference awesome?',
+              data: [5, 2, 3],
+              backgroundColor: [
+                  'rgba(135, 227, 129, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)'
+              ],
+              borderColor: [
+                  'rgba(135,227,129,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)'
+              ],
+              borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+      })
 		}.bind(this));
   }
 }
