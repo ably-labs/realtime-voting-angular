546004b7d1fcc0b67cf06f7a3c60a453455854aa
Srushtika <n.srushtika@gmail.com>
2018-02-20 18:43:22 +0100

    Full app working with realtime votes

--- ng-vote-ably/src/app/vote-chart/vote-chart.component.ts
+++ ng-vote-ably/src/app/vote-chart/vote-chart.component.ts
@@ -14,40 +14,51 @@ export class VoteChartComponent implements OnInit {
 
 
 	// Ably attributes
-	ably : any
+  ably : any
   receiveChannel : any
   chart = []
+  yes_votes: number = 0
+  no_votes: number = 0
+  maybe_votes: number = 0
+
 
 	ngOnInit() {
 		this.ably = new Ably.Realtime('6QdTiA.O9vBCw:8glD3fRTNhSEVJC5')
 
 	
 		this.receiveChannel = this.ably.channels.get('vote-channel')
-    console.log(this.receiveChannel)
+        //console.log(this.receiveChannel)
 		// Ably Subscription
-		this.receiveChannel.subscribe(function(message: any) {
-      console.log(JSON.stringify(message.data))
-      this.chart= new Chart('canvas', {
+		this.receiveChannel.subscribe("update", function(message: any) {
+        //console.log(JSON.stringify(message.data))
+        if(message.data.vote == 1)
+            this.yes_votes++
+        else if(message.data.vote == -1)
+                this.no_votes++
+        else if(message.data.vote == 0)
+                this.maybe_votes++
+        this.chart= new Chart('canvas', {
         type: 'bar',
         data: {
           labels: ["Yes", "No", "MayBe"],
           datasets: [{
-              label: 'Is this conference awesome?',
-              data: [5, 2, 3],
+              label: 'Say yes!',
+              data: [this.yes_votes, this.no_votes, this.maybe_votes],
               backgroundColor: [
-                  'rgba(135, 227, 129, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)'
+                  'rgba(52, 217, 118, 1)',
+                  'rgba(217, 63, 52, 1)',
+                  'rgba(230, 127, 0, 1)'
               ],
               borderColor: [
-                  'rgba(135,227,129,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)'
+                  'rgba(52, 217, 118, 1)',
+                  'rgba(217, 63, 52, 1)',
+                  'rgba(230, 127, 0, 1)'
               ],
               borderWidth: 1
           }]
         },
         options: {
+          animation: false,
           scales: {
               yAxes: [{
                   ticks: {
